75a07140c5ed7fe740b0287ae05207d394d313ae
German Lopez <gerleven@gmail.com>
2023-08-28 00:19:25 -0300

    update borrador

--- Repaso/00-Borrador/src/App.tsx
+++ Repaso/00-Borrador/src/App.tsx
@@ -1,12 +1,47 @@
 import './App.css';
-import { Component, Fragment } from 'react';
+import { Component, Fragment, useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [editMode, setEditMode] = useState<boolean|string>("hola");
   return (
     <>
-     
+    {loading && <Loader />}
+      
+    {editMode ? <EditForm /> : <ViewForm />}
+
+    {
+      ()=>{
+        if(editMode==true){<EditForm/>}
+        else {<ViewForm/>}
+      }
+    }
+
+
+    {/* Uso de switch dentro del método render() */}
+    {(() => {
+        switch (editMode) {
+          case true:
+            return <EditForm />;
+          case false:
+            return <ViewForm />;
+          default:
+            return <span>Error!</span>;
+        }
+      })()}
     </>
   );
 }
 
 export default App;
+
+const EditForm = ()=>(
+  <div>Edit Form</div>
+);
+
+const ViewForm = ()=>(
+  <div>View Form</div>
+);
+const Loader = ()=>(
+  <div>Loading...</div>
+);
