aaccfe5406303a64b751fe9e9fb20456396c964c
German Lopez <gerleven@gmail.com>
2023-09-01 00:15:44 -0300

    update

--- Curso Basico React/Repaso/00-General/src/Components/05-Eventos.tsx
+++ Curso Basico React/Repaso/00-General/src/Components/05-Eventos.tsx
@@ -1,24 +1,32 @@
 import {Component, Fragment, useState} from 'react';
 
-const Eventos = ()=>
+const Eventos = ({onCustomEvent, children}: any)=>
 {
     const [textValue, setTextValue] = useState<string>("Hola!");
 
-    const handleClick = ()=>{
-        console.log("You clecled me!")
-        setTextValue("You clecled me!\n");
-        
+    const handleHover = ()=>{
+        console.log("You hover me!")
+        setTextValue("You hover me!\n");
     }
-    const handleClick2= (number: number)=>{
+    const handleOut = ()=>{
+        setTextValue("Hola!\n");
+    }
+    const handleClick= (number: number)=>{
         console.log("number: "+number)
         setTextValue(prev=>(prev+"number: "+number+"\n"));
     }
+    const handleCustomEvent=()=>{
+        onCustomEvent();
+    }
 
     return <>
-    <h6>Eventos:</h6>
-    <button onClick={handleClick}>ClickMe</button>
-    <button onClick={()=>(handleClick2(2))}>Add a 2</button>
-    <p>{textValue}</p>
+    <div onMouseOver={handleHover} onMouseOut={handleOut} className='componente'>
+        <h6>Eventos:</h6>
+        <button onClick={()=>handleClick(2)}>ClickMe</button>
+        <p>{textValue}</p>
+        <button onClick={handleCustomEvent}>Custom Event</button>
+        {children}
+    </div>
     </>
 }
     
