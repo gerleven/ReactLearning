e02e49e2a713c79799225ca43f3275612d735214
German Lopez <gerleven@gmail.com>
2023-09-25 00:01:37 -0300

    tutorial react router v6 hasta Outlet y Link

--- Curso Basico React/Repaso/tutorialv6/src/main.jsx
+++ Curso Basico React/Repaso/tutorialv6/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Root from "./routes/root.jsx"
+import ErrorPage from "./error-page"
+import Contact from "./routes/contact"
+
+const myRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>,
+      },
+    ],
+  },
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={myRouter}></RouterProvider>
+    {/* <App /> */}
   </React.StrictMode>,
 )
