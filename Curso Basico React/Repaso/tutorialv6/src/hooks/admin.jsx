f3435acb8fd56a89df7fea6ba23c7d3ca2bc4886
German Lopez <gerleven@gmail.com>
2023-09-30 21:33:03 -0300

    ya funciona

--- Curso Basico React/Repaso/tutorialv6/src/hooks/admin.jsx
+++ Curso Basico React/Repaso/tutorialv6/src/hooks/admin.jsx
@@ -1,97 +1,68 @@
-import { useSubmit, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const SESSION_DURATION = 3;
+const TEST_MODE = true;
 
-export function useSessionTime(){
+//Este es el que anda bien!
+export const useSessionTime1 = () => {
     const [remainingTime, setRemainingTime] = useState(SESSION_DURATION);
-    let timer;
-
-    useEffect(()=>{
-        timer = setInterval(()=>{
-            setRemainingTime(prev=>prev-1);
-        }, 1000);
-        return ()=>clearInterval(timer)
-      },[]);
-
-
-    useEffect(()=>{
-        console.log("Timer change");
-        
-        if(remainingTime === 0){
-            console.log("Time Out!")
-            clearInterval(timer);
-        }
-      },[remainingTime]);
-    
-    return remainingTime;
-
-}
-
-
-
-
-
-
-
-export const useSessionTime2 = () => {
-    const [remainingTime, setRemainingTime] = useState(SESSION_DURATION);
-    let timer;
+    let timer = 0;
+    TEST_MODE && console.log("2)### timer declarado: "+timer)
   
     const stopTimer = () => {
-        console.log("2)### limpiar timer "+timer)
+        TEST_MODE && console.log("2)### limpiar timer "+timer)
       clearInterval(timer);
     };
   
     useEffect(() => {
       timer = setInterval(() => {
-        console.log("2)### Funcion interna del setInterval - "+timer);
+        TEST_MODE && console.log("2)### Funcion interna del setInterval - "+timer);
         setRemainingTime(prevRemainingTime => {
           if (prevRemainingTime > 0) {
             return prevRemainingTime - 1;
           } else {
             stopTimer(); // Limpiar el intervalo cuando el tiempo se agota
-            console.log('2)### ¡Tiempo agotado!');
+            TEST_MODE && console.log('2)### ¡Tiempo agotado!'+timer);
             return 0;
           }
         });
       }, 1000);
-  
+      TEST_MODE && console.log("2)### Seteado un timer id: "+timer)
+
       return stopTimer; // Limpiar el intervalo al desmontar el componente
     }, []);
   
     return remainingTime;
   };
 
-export const useSessionTime3 = () => {
+//useSessionTime con un segundo useEffect.
+export const useSessionTime2 = () => {
   const [remainingTime, setRemainingTime] = useState(SESSION_DURATION);
-  let timer; // Declaramos timer en el ámbito superior
-
-  const stopTimer = () => {
-    console.log("3)### limpiar timer "+timer)
+  const [timerId, setTimerId] = useState(0);
+  
+  const stopTimer = (timer) => {
+    TEST_MODE && console.log('3)### Limpiar el timer de id: '+timer);
     clearInterval(timer);
   };
 
   // Primer useEffect para restar 1 segundo
   useEffect(() => {
-    timer = setInterval(() => {
-        console.log("3)### Funcion interna del setInterval - "+timer);
-      setRemainingTime(prevRemainingTime => {
-        if (prevRemainingTime > 0) {
-          return prevRemainingTime - 1;
-        }
-        return prevRemainingTime;
-      });
+    let timer = setInterval(() => {
+      TEST_MODE && console.log('3)### Intervalo corriendo: '+timer);
+      setRemainingTime(prevRemainingTime => (prevRemainingTime - 1));
     }, 1000);
+    setTimerId(timer);
+    //Ojo! React ejecuta 2 veces el useEffect(f, []) para asegurarse que hiciste bien el sañamiento
+    //Este setTimerId(timer) es asincronico, por lo tanto timerId sigue valiendo 0 cuando se ejecute el stopTimer(), por eso hay que pasarle el valor timer para que el sañamiento se haga correctamente entre el 1er y 2do useEffect(f, [])
 
-    return () => clearInterval(timer); // Limpiar el intervalo al desmontar el componente
+    return () => {stopTimer(timer);} 
   }, []);
 
   // Segundo useEffect para comprobar si el tiempo se ha agotado
   useEffect(() => {
-    if (remainingTime === 0) {
-      console.log('3)### ¡Tiempo agotado!'+timer);
-      stopTimer(); // Limpia el intervalo cuando el tiempo se agota
+    if (remainingTime < 1) {
+      TEST_MODE && console.log('3)### ¡Tiempo agotado!'+timerId);
+      stopTimer(timerId); // Limpia el intervalo cuando el tiempo se agota
     }
   }, [remainingTime]);
 
--- Curso Basico React/Repaso/tutorialv6/src/routes/root.jsx
+++ Curso Basico React/Repaso/tutorialv6/src/routes/root.jsx
@@ -1,79 +1,113 @@
-import { Outlet, Link, NavLink, useLoaderData, useActionData, Form, redirect, useNavigation, useSubmit  } from "react-router-dom";
-import { getContacts, createContact } from "../contacts"
+import {Outlet,Link,NavLink,useLoaderData,useActionData,Form,redirect,useNavigation,useSubmit,} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 import { useState, useEffect } from "react";
-import {useSessionTime, useSessionTime2, useSessionTime3} from "../hooks/admin";
+import {useSessionTime1,useSessionTime2,} from "../hooks/admin";
 
 //Esta funcion sera invocada cuando el usuario acceda a la ruta "/" para cargar de manera asincronica los contactos que luego usamos para generar los Links de manera dinamica
-export async function loader({request}){
-    const url = new URL(request.url); //Esta request es enviado por el <Form> del search
-    const searchParams = url.searchParams.get("q") || "";
-    const contacts = await getContacts(searchParams);
-    return { contacts, searchParams };
+export async function loader({ request }) {
+  const url = new URL(request.url); //Esta request es enviado por el <Form> del search
+  const searchParams = url.searchParams.get("q") || "";
+  const contacts = await getContacts(searchParams);
+  return { contacts, searchParams };
 }
 
 //Gracias a este action y al <Form method="post"> podemos hacer el post para crear un nuevo contacto sin tener que usar useState, ni useEffect ni fetch(url, post) ni nada.
 export async function action() {
-    const contact = await createContact();
-    return redirect(`/contacts/${contact.id}/edit`);
-  }
-
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
 
 export default function Root() {
-    const {contacts, searchParams} = useLoaderData();
-    const navigation = useNavigation(); //navigation has this props: [state, location, formData, json, text, formAction, formMethod]
-    const submit = useSubmit();
-    // const remainingTime = useSessionTime();
-    const remainingTime2 = useSessionTime2();
-    const remainingTime3 = useSessionTime3();
+  const { contacts, searchParams } = useLoaderData();
+  const navigation = useNavigation(); //navigation has this props: [state, location, formData, json, text, formAction, formMethod]
+  const submit = useSubmit();
 
-    
-    //Una forma de mantener sincronizado el value del Search con el searchParams de la URL seria esta:
-    // const [searchValue, setSearchValue] = useState("");
-    // useEffect(()=>{
-    //   setSearchValue(searchParams==null?"":searchParams);
-    // },[searchParams]);
-    //value={searchValue}
-    //onChange={(e)=>{setSearchValue(e.target.value)}}
-
-    //Pero de esta forma no ahorramos la variable de estado y tener que poner el value y el onChange en el Form:
-    useEffect(()=>{
-      document.getElementById("searchNameInput").value=searchParams;
-    },[searchParams]);
-
-    return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            {/* Este Form no tiene method entonces por defecto la request que manda es un get (la recibe el loader) y como no tiene action=<path> lo manda a esta misma ruta donde fue renderizado*/}
-            <Form id="search-form" role="search"
-              onChange={(event)=>submit(event.currentTarget)} //El <Form> tambien tiene un onChange, con un submit dentro del onChange se haria un submit por cada cambio, y por lo tanto, la lista filtrada en tiempo real
-            >
-              <input
-                id="searchNameInput"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </Form>
-            <Form method="post">
-                <button type="submit">New</button>
-            </Form>
-          </div>
+  //Pero de esta forma no ahorramos la variable de estado y tener que poner el value y el onChange en el Form:
+  useEffect(() => {
+    document.getElementById("searchNameInput").value = searchParams;
+  }, [searchParams]);
 
 
-          <nav>
-            {/* <ul>
+
+
+  //----------- TEST
+  // const SESSION_DURATION = 300;
+  // const [remainingTime, setRemainingTime] = useState(SESSION_DURATION);
+  // const [timerId, setTimerId] = useState(0);
+  
+  
+
+  // const stopTimer = () => {
+  //   console.log("4)### limpiar timer " + timerId);
+  //   clearInterval(timerId);
+  // };
+
+  // const setTheInterval = ()=>{
+  //   let timer = setInterval(() => {
+  //     console.log("4)### Funcion interna del setInterval - " + timerId);
+  //     setRemainingTime((prevRemainingTime) => {
+  //       if (prevRemainingTime > 0) {
+  //         return prevRemainingTime - 1;
+  //       }
+  //       return prevRemainingTime;
+  //     });
+  //   }, 1000);
+  //   setTimerId(timer);
+  //   console.log("4)### timer seted: "+timerId);
+  // }
+
+  // useEffect(() => {
+  //   setTheInterval();
+  //   return () => clearInterval(timerId); // Limpiar el intervalo al desmontar el componente
+  // }, []);
+
+  // // Segundo useEffect para comprobar si el tiempo se ha agotado
+  // useEffect(() => {
+  //   if (remainingTime === 0) {
+  //     console.log("4)### ¡Tiempo agotado!" + timerId);
+  //     stopTimer(); // Limpia el intervalo cuando el tiempo se agota
+  //   }
+  // }, [remainingTime]);
+
+
+  // function test2() {
+  //   remainingTime;
+  //   timer;
+  // }
+  // function test() {
+  //   remainingTime;
+  //   timerId;
+  // }
+
+  let remainingTime1 = useSessionTime1();
+  // let remainingTime2 = useSessionTime2();
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          {/* Este Form no tiene method entonces por defecto la request que manda es un get (la recibe el loader) y como no tiene action=<path> lo manda a esta misma ruta donde fue renderizado*/}
+          <Form
+            id="search-form"
+            role="search"
+            onChange={(event) => submit(event.currentTarget)} //El <Form> tambien tiene un onChange, con un submit dentro del onChange se haria un submit por cada cambio, y por lo tanto, la lista filtrada en tiempo real
+          >
+            <input
+              id="searchNameInput"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {/* <ul>
               <li>
                 <Link to={`/contacts/1`}>Your Name</Link>
               </li>
@@ -88,23 +122,24 @@ export default function Root() {
                 <li key={contact.id}>
                   <NavLink
                     to={`contacts/${contact.id}`}
-                    className={({isActive, isPending})=>(isActive ? "active" : isPending? "pending" : "")}>
-                      {
-                        ({isActive,isPending})=>(
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
+                    {({ isActive, isPending }) => (
+                      <>
+                        {contact.first || contact.last ? (
                           <>
-                            {contact.first || contact.last ? (
-                            <>
-                              {contact.first} {contact.last}
-                            </>
-                            ) : (
-                              <i>No Name</i>
-                            )}{" "}
-                            {isPending && <>⏳</>}
-                            {isActive?<span>✔</span>:<></>}
-                            {contact.favorite && <span>★</span>}
+                            {contact.first} {contact.last}
                           </>
-                        )
-                      }
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {isPending && <>⏳</>}
+                        {isActive ? <span>✔</span> : <></>}
+                        {contact.favorite && <span>★</span>}
+                      </>
+                    )}
                   </NavLink>
                 </li>
               ))}
@@ -114,20 +149,31 @@ export default function Root() {
               <i>No contacts</i>
             </p>
           )}
-          </nav>
+        </nav>
+        {/* <p>Time remaining 2: {remainingTime1}</p> */}
+      </div>
 
+      <div id="detail">
+        <h1>test</h1>
+        {/* <button onClick={test}>Test</button>
+        <button onClick={test2}>Test 2</button>
+        <button onClick={stopTimer}>Stop Timer</button>
+        <button onClick={setTheInterval}>Set Interval</button> */}
+        <p>Time remaining 1: {remainingTime1}</p>
+        {/* <p>Time remaining 2: {remainingTime2}</p> */}
+      </div>
 
-          
-
-
-            {/* <p>Time remaining: {remainingTime}</p> */}
-            <p>Time remaining 2: {remainingTime2}</p>
-            <p>Time remaining 3: {remainingTime3}</p>
-        </div>
-
-        <div id="detail" className={navigation.state==="loading"?"loading":""}>
+      {/* <div id="detail" className={navigation.state==="loading"?"loading":""}>
             <Outlet/>
-        </div>
-      </>
-    );
-  }
+        </div> */}
+    </>
+  );
+}
+
+//Una forma de mantener sincronizado el value del Search con el searchParams de la URL seria esta:
+// const [searchValue, setSearchValue] = useState("");
+// useEffect(()=>{
+//   setSearchValue(searchParams==null?"":searchParams);
+// },[searchParams]);
+//value={searchValue}
+//onChange={(e)=>{setSearchValue(e.target.value)}}
